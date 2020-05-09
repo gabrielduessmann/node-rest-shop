c2fac51fe3a3f3b9926c5c8b98ce06f81bca80d1
Gabriel Duessmann <gabriel.duessmann@gmail.com>
2020-05-09 10:16:00 -0300

    Morgan and handled error

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-const express = require('express');
-
 // run express
+const express = require('express');
 const app = express();
 
-// product
+// run morgan
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+// product 
 const producstRoutes = require('./api/routes/products');
 app.use('/products', producstRoutes);
 
 // orders
 const orderRoutes = require('./api/routes/orders');
 app.use('/orders', orderRoutes);
+
+//handle error
+app.use((req, res, next) => {
+    const error = new Error('Request not found');
+    error.status = 404;
+    next(error);
+});
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;

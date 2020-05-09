90d67df83b6e795a96b313401e3b66787cc44452
Gabriel Duessmann <gabriel.duessmann@gmail.com>
2020-05-09 11:15:58 -0300

    Handling CORS error

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Acess-Control-Allow-Origin', '*');
+    res.header('Acess-Control-Allow-Headers', 
+            'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods',
+            'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 // product 
 const producstRoutes = require('./api/routes/products');
 app.use('/products', producstRoutes);

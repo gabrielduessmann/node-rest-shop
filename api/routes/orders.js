fc69e3d39b1b41dda346d45cc624de3b4ebb1c75
Gabriel Duessmann <gabriel.duessmann@gmail.com>
2020-05-31 20:44:54 -0300

    Order Controller Layer

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,12 +1,5 @@
 const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
-
-const Order = require('../models/order');
-const Product = require('../models/product');
-
 const checkAuth = require('../middleware/check-auth');
-
 const OrdersController  = require('../controllers/orders'); 
 
 router.get('/', checkAuth, OrdersController.orders_get_all);

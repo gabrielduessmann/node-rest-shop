fb18b0533e6833ad7aff6d1bc2468c66c72adec9
Gabriel Duessmann <gabriel.duessmann@gmail.com>
2020-05-30 23:19:22 -0300

    Adding user login

--- api/routes/user.js
+++ api/routes/user.js
@@ -51,6 +51,40 @@ router.post('/signup', (req, res, next) => {
     })
 });
 
+router.post("/login", (req, res, next) => {
+    User.findOne({email: req.body.email})
+    .exec()
+    .then(user => {
+        if(user.length<1) {
+            return res.status(401).json({
+                message: "Auth failed"
+            });
+        }
+        bcrypt.compare(req.body.password, user.password, (err, result) =>{
+            if (err) {
+                return res.status(404).json({
+                    message: "Auth failed."
+                });
+            }
+            if (result) {
+                return res.status(200).json({
+                    message: "Auth successful"
+                });
+            } else {
+                return res.status(404).json({
+                    message: "Auth failed."
+                });
+            }
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
 router.delete('/:userId', (req, res, next) => {
     User.remove({
         _id: req.params.userId
